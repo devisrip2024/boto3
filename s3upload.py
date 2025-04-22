432269f155c3331e30c26b4c6bebd6e2e25711da
DevOpsG <76554480+DevSecOpsG@users.noreply.github.com>
2025-04-22 23:10:42 +0530

    Update s3upload.py

--- s3upload.py
+++ s3upload.py
@@ -1,3 +1,3 @@
 import boto3
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file(r"C:\Users\Admin\Desktop\batch code\boto3\hello.txt" , 'elasticbeanstalk-us-east-2-878088820643', 'hello.txt')
+s3.meta.client.upload_file(r"C:\Users\cloud\OneDrive\Desktop\batchcode's\boto3" , 'elasticbeanstalk-us-east-2-878088820643', 'hello.txt')
